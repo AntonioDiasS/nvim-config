2d46084f1e90151b8a49520caa8092bdeb9bd45e
AntonioDiasS <antoniodias0703@gmail.com>
2025-07-30 17:40:38 -0300

    Adiciona Telescope

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+-- Improves syntax highlighting and code analysis.
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,45 @@
+-- Plugins que melhoram a interface do usuário e a experiência de uso.
+return {
+  -- Mostra os próximos atalhos de teclado possíveis.
+  {
+    "folke/which-key.nvim",
+    event = "VimEnter",
+    opts = {
+      delay = 0,
+      spec = {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      },
+    },
+  },
+
+  -- Destaca TODO, NOTE, FIXME, etc., nos comentários.
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = false },
+  },
+
+  -- Coleção de pequenos plugins. Usamos para:
+  -- - Melhores text objects (ex: `va)` para selecionar ao redor de parênteses)
+  -- - Adicionar/remover/substituir "arredores" (ex: `ds'` para deletar aspas simples)
+  -- - Uma statusline simples e bonita.
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.ai").setup({ n_lines = 500 })
+      require("mini.surround").setup()
+      local statusline = require("mini.statusline")
+      statusline.setup({ use_icons = vim.g.have_nerd_font })
+      statusline.section_location = function()
+        return "%2l:%-2v"
+      end
+    end,
+  },
+}
