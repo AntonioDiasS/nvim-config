ce7df1346a695b8d522a6838b99438f66d132cc6
AntonioDiasS <antoniodias0703@gmail.com>
2026-02-02 12:06:36 -0300

    Correção do treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,41 @@
--- Improves syntax highlighting and code analysis.
 return {
   "nvim-treesitter/nvim-treesitter",
+  version = false, -- Usar a versão master (releases são muito antigos)
   build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" }, -- Carrega apenas ao abrir um arquivo
   opts = {
-    ensure_installed = { 
+    -- Uma lista de parsers para instalar obrigatoriamente
+    ensure_installed = {
+      "bash",
       "c",
+      "html",
+      "javascript",
+      "json",
       "lua",
+      "python",
+      "query",
+      "typescript",
       "vim",
       "vimdoc",
-      "query",
-      "javascript",
-      "typescript",
-      "python",
-      "html",
-      "css",
-      "go",
+      "yaml",
     },
+    -- Instala parsers automaticamente ao abrir arquivos novos
     auto_install = true,
-    highlight = { enable = true },
+    
+    -- Ativa o realce de sintaxe (Highlighting)
+    highlight = {
+      enable = true,
+      -- Desabilitar para arquivos muito grandes para não travar
+      disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+              return true
+          end
+      end,
+    },
+    
+    -- Ativa a indentação baseada no treesitter (melhor que a padrão)
     indent = { enable = true },
   },
   config = function(_, opts)
